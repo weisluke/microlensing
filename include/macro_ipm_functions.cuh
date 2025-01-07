1437810351e365022f469da9c0d202e56d300664
Luke Weisenbach <luke.weisenbach@port.ac.uk>
2025-01-06 22:19:09 -0500

    update rayshooting functions

--- include/macro_ipm_functions.cuh
+++ include/macro_ipm_functions.cuh
@@ -1,11 +1,13 @@
 #pragma once
 
 #include "alpha_local.cuh"
+#include "alpha_macro.cuh"
 #include "alpha_smooth.cuh"
 #include "alpha_star.cuh"
 #include "array_functions.cuh"
 #include "complex.cuh"
 #include "polygon.cuh"
+#include "potential.cuh"
 #include "star.cuh"
 #include "tree_node.cuh"
 #include "util.cuh"
@@ -15,8 +17,7 @@
 lens equation from image plane to source plane
 
 \param z -- complex image plane position
-\param kappa -- total convergence
-\param gamma -- external shear
+\param p -- macro-model derivatives of the potential
 \param theta -- size of the Einstein radius of a unit mass point lens
 \param stars -- pointer to array of point mass lenses
 \param kappastar -- convergence in point mass lenses
@@ -28,29 +29,29 @@ lens equation from image plane to source plane
 \param taylor_smooth -- degree of the taylor series for alpha_smooth if
 						approximate
 
-\return w = (1 - kappa) * z + gamma * z_bar
-			- alpha_star - alpha_local - alpha_smooth
+\return w = z - alpha_macro - alpha_star - alpha_local - alpha_smooth
 ******************************************************************************/
 template <typename T>
-__device__ Complex<T> complex_image_to_source(Complex<T> z, T kappa, T gamma, T theta, star<T>* stars, T kappastar, TreeNode<T>* node,
+__device__ Complex<T> complex_image_to_source(Complex<T> z, potential<T> p, 
+	T theta, star<T>* stars, T kappastar, TreeNode<T>* node,
 	int rectangular, Complex<T> corner, int approx, int taylor_smooth)
 {
+	Complex<T> a_macro = alpha_macro<T>(z, p);
 	Complex<T> a_star = alpha_star<T>(z, theta, stars, node);
 	Complex<T> a_local = alpha_local<T>(z, theta, node);
 	Complex<T> a_smooth = alpha_smooth<T>(z, kappastar, rectangular, corner, approx, taylor_smooth);
 
 	/******************************************************************************
-	(1 - kappa) * z + gamma * z_bar - alpha_star - alpha_local - alpha_smooth
+	z - alpha_macro - alpha_star - alpha_local - alpha_smooth
 	******************************************************************************/
-	return (1 - kappa) * z + gamma * z.conj() - a_star - a_local - a_smooth;
+	return z - a_macro - a_star - a_local - a_smooth;
 }
 
 /******************************************************************************
 magnification at a point in the image plane
 
 \param z -- complex image plane position
-\param kappa -- total convergence
-\param gamma -- external shear
+\param p -- macro-model derivatives of the potential
 \param theta -- size of the Einstein radius of a unit mass point lens
 \param stars -- pointer to array of point mass lenses
 \param kappastar -- convergence in point mass lenses
@@ -65,16 +66,22 @@ magnification at a point in the image plane
 \return mu = ( (dw / dz)^2 - dw/dz * (dw/dz)bar ) ^ -1
 ******************************************************************************/
 template <typename T>
-__device__ T magnification(Complex<T> z, T kappa, T gamma, T theta, star<T>* stars, T kappastar, TreeNode<T>* node,
+__device__ T magnification(Complex<T> z, potential<T> p, 
+	T theta, star<T>* stars, T kappastar, TreeNode<T>* node,
 	int rectangular, Complex<T> corner, int approx, int taylor_smooth)
 {
+	T d_a_macro_d_z = d_alpha_macro_d_z<T>(z, p);
+	Complex<T> d_a_macro_d_zbar = d_alpha_macro_d_zbar<T>(z, p);
+
 	Complex<T> d_a_star_d_zbar = d_alpha_star_d_zbar<T>(z, theta, stars, node);
+
 	Complex<T> d_a_local_d_zbar = d_alpha_local_d_zbar<T>(z, theta, node);
+
 	T d_a_smooth_d_z = d_alpha_smooth_d_z<T>(z, kappastar, rectangular, corner, approx);
 	Complex<T> d_a_smooth_d_zbar = d_alpha_smooth_d_zbar<T>(z, kappastar, rectangular, corner, approx, taylor_smooth);
 
-	T d_w_d_z = (1 - kappa) - d_a_smooth_d_z;
-	Complex<T> d_w_d_zbar = gamma - d_a_star_d_zbar - d_a_local_d_zbar - d_a_smooth_d_zbar;
+	T d_w_d_z = 1 - d_a_macro_d_z - d_a_smooth_d_z;
+	Complex<T> d_w_d_zbar = -d_a_macro_d_zbar - d_a_star_d_zbar - d_a_local_d_zbar - d_a_smooth_d_zbar;
 
 	T mu_inv = d_w_d_z * d_w_d_z - d_w_d_zbar.abs() * d_w_d_zbar.abs();
 
@@ -84,8 +91,7 @@ __device__ T magnification(Complex<T> z, T kappa, T gamma, T theta, star<T>* sta
 /******************************************************************************
 shoot rays from image plane to source plane
 
-\param kappa -- total convergence
-\param gamma -- external shear
+\param p -- macro-model derivatives of the potential
 \param theta -- size of the Einstein radius of a unit mass point lens
 \param stars -- pointer to array of point mass lenses
 \param kappastar -- convergence in point mass lenses
@@ -110,7 +116,7 @@ shoot rays from image plane to source plane
 \param verbose -- verbose level
 ******************************************************************************/
 template <typename T>
-__global__ void shoot_cells_kernel(T kappa, T gamma, T theta, star<T>* stars, T kappastar, TreeNode<T>* root,
+__global__ void shoot_cells_kernel(potential<T> p, T theta, star<T>* stars, T kappastar, TreeNode<T>* root,
 	int rectangular, Complex<T> corner, int approx, int taylor_smooth,
 	Complex<T> ray_half_sep, Complex<int> num_ray_threads, Complex<T> center_x, Complex<T> hlx,
 	Complex<T> center_y, Complex<T> hly, T* pixmin, T* pixsad, T* pixels, Complex<int> npixels, 
@@ -134,7 +140,7 @@ __global__ void shoot_cells_kernel(T kappa, T gamma, T theta, star<T>* stars, T 
 			for (int a = 0; a < 4; a++)
 			{
 				TreeNode<T>* node = treenode::get_nearest_node(x[a], root);
-				y[a] = complex_image_to_source(x[a], kappa, gamma, theta, stars, kappastar, node, rectangular, corner, approx, taylor_smooth);
+				y[a] = complex_image_to_source(x[a], p, theta, stars, kappastar, node, rectangular, corner, approx, taylor_smooth);
 				/******************************************************************************
 				if the ray location is the same as a star position, we will have a nan returned
 				******************************************************************************/
