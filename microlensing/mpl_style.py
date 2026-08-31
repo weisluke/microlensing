cbcf392ea60ebc9f9775cbc600906857e1185747
Luke Weisenbach <weisluke@alum.mit.edu>
2026-08-31 19:03:29 -0400

    update style file

--- microlensing/mpl_style.py
+++ microlensing/mpl_style.py
@@ -1,19 +1,16 @@
 # see http://matplotlib.org/users/customizing.html for all options
 
 style = {
-    # Line styles
-    'lines.linewidth': 1.5,
-    'lines.antialiased': True,
-
     # Font
     'font.size': 16.0,
+
+    # Latex
+    'text.usetex': True,
 
     # Axes
     'axes.linewidth': 1.5,
     'axes.titlesize': 'x-large',
     'axes.labelsize': 'large',
-    'axes.spines.top': True,
-    'axes.spines.right': True,
 
     # Ticks
     'xtick.major.size': 6,
@@ -22,7 +19,6 @@ style = {
     'xtick.minor.width': 1.5,
     'xtick.major.pad': 6,
     'xtick.minor.pad': 6,
-    'xtick.labelsize': 'medium',
     'xtick.direction': 'in',
     'xtick.top': True,
 
@@ -32,25 +28,13 @@ style = {
     'ytick.minor.width': 1.5,
     'ytick.major.pad': 6,
     'ytick.minor.pad': 6,
-    'ytick.labelsize': 'medium',
     'ytick.direction': 'in',
     'ytick.right': True,
 
-    # Legend
-    'legend.fancybox': True,
-    'legend.fontsize': 'medium',
-    'legend.scatterpoints': 5,
-    'legend.loc': 'best',
-
     # Figure
-    'figure.figsize': [8, 6],
-    'figure.titlesize': 'large',
-
-    # Images
-    'image.cmap': 'viridis',
-    'image.origin': 'upper',
+    'figure.figsize': [8, 5],
 
     # Saving
     'savefig.bbox': 'tight',
-    'savefig.format': 'png',
+    'savefig.format': 'pdf',
 }
